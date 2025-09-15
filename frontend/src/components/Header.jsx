60558580f94a98ebf7cf1d30aa2877c08f585cc3
Гамзат <abdullayevgamzat@gmail.com>
2025-09-16 00:45:57 +0300

    again create project

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+
+
+const Header = () => {
+    return (
+        <header className="header">
+        <div className="header__container">
+          <img src="/img/icons/logo.svg" alt="" className="logo" />
+          <nav className="nav__menu">
+            <button><img src="/img/icons/country.svg" alt="" />Москва</button>
+            <ul className="menu">
+              <li className="menu__item"><a href="" className="menu__link">Главная</a></li>
+              <li className="menu__item"><a href="" className="menu__link">Доставка</a></li>
+              <li className="menu__item"><a href="" className="menu__link">О нас</a></li>
+              <li className="menu__item"><a href="" className="menu__link">Новости</a></li>
+              <li className="menu__item">
+                <a href="" className="menu__link menu__number">
+                  <img src="/img/icons/phone.svg" alt="" />
+                  <span>+38 097 699 34 38</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="block__interaction">
+            <button className="interaction-btn"><img src="/img/icons/bell.svg" alt="" /></button>
+            <button className="interaction-btn"><img src="/img/icons/heart.svg" alt="" /></button>
+            <button className="interaction-btn"><img src="/img/icons/profile.svg" alt="" /></button>
+            <button className="interaction-btn cart-btn">
+              <span>Корзина</span>
+              <img src="/img/icons/cart.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </header>
+    );
+}
+
+export default Header;
